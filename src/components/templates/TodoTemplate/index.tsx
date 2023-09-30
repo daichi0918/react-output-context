09c529f51d53022b49eb68f886b75ec6def80c10
daichi0918 <kayasugadaichi@icloud.com>
2023-09-30 16:03:05 +0900

    枠組み作成

---
+++ src/components/templates/TodoTemplate/index.tsx
@@ -0,0 +1,27 @@
+
+
+export const TodoTemplate = () => {
+
+  return(
+    <div>
+      <h1>Todo List</h1>
+      <section>
+        <h2>ADD TODO</h2>
+        <input type="text" placeholder="New Todo"/>
+      </section>
+      <section>
+        <input />
+      </section>
+      <section>
+        <ul>
+          <li>
+            Todo1
+          </li>
+          <li>
+            Todo2
+          </li>
+        </ul>
+      </section>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Todo } from './pages/Todo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 
